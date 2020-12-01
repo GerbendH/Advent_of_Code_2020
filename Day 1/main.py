bec49c4010535b70eb48d64828ad46ca7be70f35
Gerben <gerben@ideetron.nl>
2020-12-01 18:29:44 +0100

    Day one done

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+def get_report():
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+    report_int = []
+
+    report_input_file = open("input.txt", "r")
+    if report_input_file.mode == "r":
+        report_string = report_input_file.read()
+        weight_string_split = report_string.split("\n")
+
+        for weight in weight_string_split:
+            report_int.append(int(weight))
+
+    return report_int
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def anylysor(report):
+
+    result = 0
+
+    for entry_1 in report:
+        for entry_2 in report:
+            if entry_1 != entry_2:
+                for entry_3 in report:
+                    if entry_3 != entry_1 and entry_3 != entry_2:
+                        if (entry_1 + entry_2 + entry_3 ) == 2020:
+                            result = entry_1 * entry_2 * entry_3
+                            print(entry_1)
+                            print(entry_2)
+                            print(entry_3)
+                            print(result)
+
+    return result
 
 
-# Press the green button in the gutter to run the script.
+def analyse_report():
+
+    report = get_report()
+    result = anylysor(report)
+
+    print(result)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    analyse_report()
