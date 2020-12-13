18a2234323ff971c6c651b8034c2be175ca718f8
Gerben <gerben@ideetron.nl>
2020-12-13 11:20:11 +0100

    Day 12 part 2 done

--- Day 12/main.py
+++ Day 12/main.py
@@ -16,7 +16,7 @@ def move_ship(loc, direction, distance):
         loc[0] -= distance
 
 
-def handle_instruction(instruction, location, ship_direction):
+def handle_instruction_part1(instruction, location, ship_direction):
     action = instruction[0]
     distance = int(instruction[1:])
 
@@ -46,17 +46,70 @@ def handle_instruction(instruction, location, ship_direction):
 
     return ship_direction
 
+def rotate_right(waypoint):
+    x = waypoint[0]
+    y = waypoint[1]
+
+    waypoint[0] = y
+    waypoint[1] = x * -1
+
+
+def rotate_left(waypoint):
+    x = waypoint[0]
+    y = waypoint[1]
+
+    waypoint[0] = y * -1
+    waypoint[1] = x
+
+
+def handle_instruction_part2(instruction, waypoint, ship):
+    action = instruction[0]
+    distance = int(instruction[1:])
+
+    if action == 'N':
+        waypoint[1] += distance
+    elif action == 'E':
+        waypoint[0] += distance
+    elif action == 'S':
+        waypoint[1] -= distance
+    elif action == 'W':
+        waypoint[0] -= distance
+    elif action == 'F':
+        ship[0] += distance * waypoint[0]
+        ship[1] += distance * waypoint[1]
+
+    if action == 'R':
+        if distance == 180:
+            waypoint[0] = waypoint[0] * -1
+            waypoint[1] = waypoint[1] * -1
+        elif distance == 90:
+            rotate_right(waypoint)
+        elif distance == 270:
+            rotate_left(waypoint)
+
+    if action == 'L':
+        if distance == 180:
+            waypoint[0] = waypoint[0] * -1
+            waypoint[1] = waypoint[1] * -1
+        elif distance == 90:
+            rotate_left(waypoint)
+        elif distance == 270:
+            rotate_right(waypoint)
+
 
 def risk_of_rain():
     instruction_file = "input.txt"
     navigation_instructions = load_file_readlines(instruction_file)
+    waypoint = [10, 1]
     ship_location = [0, 0]
     ship_direction = 90  # N = 0, E = 90, S = 180, W = 270
 
     for instruction in navigation_instructions:
-        ship_direction = handle_instruction(instruction, ship_location, ship_direction)
+        # ship_direction = handle_instruction_part1(instruction, ship_location, ship_direction)
+        handle_instruction_part2(instruction, waypoint, ship_location)
 
-    print(abs(ship_location[0] + abs(ship_location[1])))
+
+    print(ship_location)
 
 
 if __name__ == '__main__':
